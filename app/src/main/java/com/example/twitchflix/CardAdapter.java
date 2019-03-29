e63e1a7ae173c9795655e31a17782ff893dabfd0
Leonardo Santos <leofbsantos@gmail.com>
2019-03-29 15:10:29 +0000

    Added onItemClickListener for recycler View, used cardview as reference, probably going to change that later

--- app/src/main/java/com/example/twitchflix/CardAdapter.java
+++ app/src/main/java/com/example/twitchflix/CardAdapter.java
@@ -1,30 +1,58 @@
 package com.example.twitchflix;
 
 import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class CardAdapter extends RecyclerView.Adapter <CardAdapter.MyViewHolder>{
 
+    // recycler view has no default onclicklistener
+    // create interface to implement own onClick to each cardView
+    public interface OnItemClickListener{
+         void onItemClick(CardView cardView);
+    }
+
     // Provide a reference to the views for each data item
     private String[] mFilm = {"Title 1", "Title 2", "Title 3", "Title 4", "Title 5", "Title 6", "Title 7", "Title 8", "Title 9" };
-    private ImageView mImage;
+    private OnItemClickListener listener;
 
+    public CardAdapter(OnItemClickListener listener){
+        this.listener = listener;
+    }
 
 
     public static class MyViewHolder extends RecyclerView.ViewHolder{
         public TextView title;
         public ImageView image;
+        public CardView cardView;
 
         public MyViewHolder(View itemView) {
             super(itemView);
             this.title = (TextView) itemView.findViewById(R.id.film_title);
             this.image = (ImageView) itemView.findViewById(R.id.film_image);
+            this.cardView = itemView.findViewById(R.id.card_view);
         }
+
+
+         // bind the data to its place inside the cardView and set the onClickListener
+         public void bind(String film_title, int film_image, final CardView cardview, final OnItemClickListener listener){
+             title.setText(film_title);
+             image.setImageResource(film_image);
+
+             itemView.setOnClickListener(new View.OnClickListener() {
+                 @Override
+                 public void onClick(View v) {
+                     listener.onItemClick(cardview);
+                 }
+             });
+         }
+
     }
 
     // Create new views (invoked by the layout manager)
@@ -38,9 +66,9 @@ public class CardAdapter extends RecyclerView.Adapter <CardAdapter.MyViewHolder>
 
     // Replace the contents of a view (invoked by the layout manager)
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i) {
-        myViewHolder.title.setText(mFilm[i]);
-        myViewHolder.image.setImageResource(R.drawable.vod_icon);
+    public void onBindViewHolder(@NonNull final MyViewHolder myViewHolder, int i) {
+        // bind takes the data for each cardview, a reference to the cardview and the listener
+        myViewHolder.bind(mFilm[i], R.drawable.vod_icon, myViewHolder.cardView, listener);
 
     }
 
@@ -49,4 +77,6 @@ public class CardAdapter extends RecyclerView.Adapter <CardAdapter.MyViewHolder>
     public int getItemCount() {
         return mFilm.length;
     }
+
+
 }
--- app/src/main/java/com/example/twitchflix/VodsFragment.java
+++ app/src/main/java/com/example/twitchflix/VodsFragment.java
@@ -1,18 +1,16 @@
 package com.example.twitchflix;
 
-import android.content.Intent;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ListView;
+import android.widget.Toast;
 
 public class VodsFragment extends Fragment {
 
@@ -28,11 +26,18 @@ public class VodsFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_vods, container, false);
 
         recyclerView = (RecyclerView) rootView.findViewById(R.id.recycler_view_vods);
-
         layoutManager = new LinearLayoutManager(getContext());
         recyclerView.setLayoutManager(layoutManager);
 
-        mAdapter = new CardAdapter(); // adapter to the card based layout
+        // new Card Adapter with a onItemClickListener
+        mAdapter = new CardAdapter(new CardAdapter.OnItemClickListener(){
+            @Override
+            public void onItemClick(CardView cardView) {
+                Toast.makeText(getContext(), "Item Clicked", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        // adapter to the card based layout
         recyclerView.setAdapter(mAdapter);
 
 
