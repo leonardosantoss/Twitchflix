de9b63c9f6372e36d1126e654b239dc893036ff0
Daniel <daniel.zheng.dong@gmail.com>
2019-05-10 17:57:40 +0100

    connection do the database

--- server/src/main/java/server/server/MyResource.java
+++ server/src/main/java/server/server/MyResource.java
@@ -1,5 +1,15 @@
 package server.server;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -10,7 +20,15 @@ import javax.ws.rs.core.MediaType;
  */
 @Path("myresource")
 public class MyResource {
+    // JDBC driver name and database URL
 
+    static final String JDBC_DRIVER = "org.mariadb.jdbc.Driver";
+    static final String DB_URL = "jdbc:mariadb://35.240.44.105:3306/twitchflixdb";
+
+    //  Database credentials
+    static final String USER = "root";
+    static final String PASS = "mariadbroot";
+    
     /**
      * Method handling HTTP GET requests. The returned object will be sent
      * to the client as "text/plain" media type.
@@ -18,8 +36,61 @@ public class MyResource {
      * @return String that will be returned as a text/plain response.
      */
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getIt() {
-        return "Got it!";
+    @Produces(MediaType.APPLICATION_JSON)
+    public JsonArray getIt() {
+    	JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
+    	JsonObject obj = null;
+    	Connection conn = null;
+        Statement stmt = null;
+        try {
+            //STEP 2: Register JDBC driver
+            Class.forName("org.mariadb.jdbc.Driver");
+
+            //STEP 3: Open a connection
+            System.out.println("Connecting to a selected database...");
+            conn = DriverManager.getConnection( DB_URL, USER, PASS);
+            System.out.println("Connected database successfully...");
+
+            //STEP 4: Execute a query
+            System.out.println("Retrieving from database...");
+            stmt = conn.createStatement();
+
+            String sql = "Select * from movies";
+
+            ResultSet rs = stmt.executeQuery(sql);
+            System.out.println("Retrieved from database...");
+            
+            while (rs.next()) {
+            	obj = Json.createObjectBuilder()
+        			.add("id", rs.getInt("idmovies"))
+        			.add("name", rs.getString("name"))
+        			.add("link", rs.getString("link"))
+        			.build();
+            	arrayBuilder.add(obj);
+            }
+        } catch (SQLException se) {
+            //Handle errors for JDBC
+            se.printStackTrace();
+        } catch (Exception e) {
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        } finally {
+            //finally block used to close resources
+            try {
+                if (stmt != null) {
+                    conn.close();
+                }
+            } catch (SQLException se) {
+            }// do nothing
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }//end finally try
+        }//end try
+        System.out.println("Goodbye!");
+        return arrayBuilder.build();
     }
 }
