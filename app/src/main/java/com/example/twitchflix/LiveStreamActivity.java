82d6b219e173cae5a95e7c17edced7e25f7c6f72
Leonardo Santos <leofbsantos@gmail.com>
2019-05-31 19:51:06 +0100

    Stream Works, added buttons to swap camera and start/stop Stream. Permissions for camera and microphone usage are not well done, needs to be revised

--- app/src/main/java/com/example/twitchflix/LiveStreamActivity.java
+++ app/src/main/java/com/example/twitchflix/LiveStreamActivity.java
@@ -9,29 +9,30 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
+import com.pedro.encoder.input.video.CameraOpenException;
 import com.pedro.rtplibrary.rtmp.RtmpCamera1;
 
 import net.ossrs.rtmp.ConnectCheckerRtmp;
 
+public class LiveStreamActivity extends AppCompatActivity implements ConnectCheckerRtmp, SurfaceHolder.Callback {
 
-public class LiveStreamActivity extends AppCompatActivity {
-
+    RtmpCamera1 rtmpCamera1;
     SurfaceView surfaceView;
-    ConnectCheckerRtmp connectCheckerRtmp;
     String username, url = null;
+    Button button_live_stream, button_swap_camera;
     private static final int MY_CAMERA_REQUEST_CODE = 100;
     private static final int MY_AUDIO_REQUEST_CODE = 100;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_live_stream);
-        surfaceView = findViewById(R.id.surfaceViewLiveStream);
-
 
         if (savedInstanceState == null) {
             Bundle extras = getIntent().getExtras();
@@ -44,71 +45,133 @@ public class LiveStreamActivity extends AppCompatActivity {
             username = (String) savedInstanceState.getSerializable("username");
         }
 
-        connectCheckerRtmp = new ConnectCheckerRtmp() {
-            @Override
-            public void onConnectionSuccessRtmp() {
-                System.out.println("Connection Successfull");
-            }
+        url = "rtmp://35.222.56.211/live/" + username;
 
-            @Override
-            public void onConnectionFailedRtmp(String reason) {
-                System.out.println("Connection Failed");
-            }
-
-            @Override
-            public void onDisconnectRtmp() {
-                System.out.println("Disconnect");
-            }
-
-            @Override
-            public void onAuthErrorRtmp() {
-                System.out.println("Auth Error");
-            }
-
-            @Override
-            public void onAuthSuccessRtmp() {
-                System.out.println("Auth Success");
-            }
-        };
+        surfaceView = findViewById(R.id.surfaceViewLiveStream);
+        surfaceView.getHolder().addCallback(this);
+        button_live_stream = findViewById(R.id.button_live_stream);
+        button_swap_camera = findViewById(R.id.button_swap_camera);
 
         // check if device has a camera
         if(checkCameraHardware(getApplicationContext())){
             // Check permission to use camera
             if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED){
                 System.out.println("Check for permission Camera");
-                //request permission
                 ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, MY_CAMERA_REQUEST_CODE);
             }
-
             if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_DENIED){
                 System.out.println("Check for permission Camera");
-                //request permission
                 ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.RECORD_AUDIO}, MY_AUDIO_REQUEST_CODE);
             }
-
-            //create builder
-            RtmpCamera1 rtmpCamera1 = new RtmpCamera1(surfaceView, connectCheckerRtmp);
-            //start stream
-            if (rtmpCamera1.prepareAudio() && rtmpCamera1.prepareVideo()) {
-                url = "rtmp://35.222.56.211/live/" + username;
-                System.out.println(url);
-                rtmpCamera1.startStream(url);
-            } else {
-                System.out.println("This device cant init encoders, this could be for 2 reasons: The encoder selected doesnt support any configuration setted or your device hasnt a H264 or AAC encoder");
-            }
-            //stop stream
-            //rtmpCamera1.stopStream();
-
+            rtmpCamera1 = new RtmpCamera1(surfaceView, this);
         }
         else {
+            Toast.makeText(this, "Device does not have a working camera!", Toast.LENGTH_SHORT).show();
             System.out.println("Device does not have a working camera!");
         }
 
 
+        button_live_stream.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!rtmpCamera1.isStreaming()){
+                    if(rtmpCamera1.isRecording() || rtmpCamera1.prepareAudio() && rtmpCamera1.prepareVideo()){
+                        rtmpCamera1.startStream(url);
+                        rtmpCamera1.startPreview();
+                    }
+                    else{
+                        System.out.println("Error preparing stream, This device cant do it");
+                    }
+                    button_live_stream.setText("Stop Stream");
 
+                }else{
+                    button_live_stream.setText("Start Stream");
+                    rtmpCamera1.stopStream();
+                }
+            }
+        });
+
+        button_swap_camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    rtmpCamera1.switchCamera();
+                } catch (CameraOpenException e) {
+                    Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
     }
 
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        rtmpCamera1.startPreview();
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+        if (rtmpCamera1.isRecording()) {
+            rtmpCamera1.stopRecord();
+        }
+        if (rtmpCamera1.isStreaming()) {
+            rtmpCamera1.stopStream();
+        }
+        rtmpCamera1.stopPreview();
+    }
+
+    @Override
+    public void onConnectionSuccessRtmp() {
+    }
+
+    @Override
+    public void onConnectionFailedRtmp(String reason) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(LiveStreamActivity.this, "Connection failed. ",
+                        Toast.LENGTH_SHORT).show();
+                rtmpCamera1.stopStream();
+
+            }
+        });
+    }
+
+    @Override
+    public void onDisconnectRtmp() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(LiveStreamActivity.this, "Disconnected", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onAuthErrorRtmp() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(LiveStreamActivity.this, "Auth error", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onAuthSuccessRtmp() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(LiveStreamActivity.this, "Auth success", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 
     private boolean checkCameraHardware(Context context) {
         if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
@@ -131,7 +194,4 @@ public class LiveStreamActivity extends AppCompatActivity {
             }
         }
     }
-
-
-
 }
